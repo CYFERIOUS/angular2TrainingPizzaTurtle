83504ede9635f082aee2a1bfa11935cd8be51f15
fernossa <fernando.nossa@publicissapient.com>
2022-08-31 15:56:07 -0500

    formsAssignmentResumeby2

--- src/app/services/logging.service.ts
+++
@@ -1,7 +0,0 @@
-export class LoggingService {
-    logStatusChanged(name: string,status:string){
-        console.log('A server status named, new name: ' + name);
-        console.log('A server status changed, new status: ' + status);
-
-    }
-}
